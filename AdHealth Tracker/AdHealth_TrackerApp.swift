9d996af9872cd8a1cb8ca8d7b4f4d7a30b18362c
Bryan Caro <bryan_caro@carrefour.com>
2023-02-26 19:29:09 +0100

    Initial Commit

---
+++ AdHealth Tracker/AdHealth_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdHealth_TrackerApp.swift
+//  AdHealth Tracker
+//
+//  Created by Bryan Caro on 26/2/23.
+//
+
+import SwiftUI
+
+@main
+struct AdHealth_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
