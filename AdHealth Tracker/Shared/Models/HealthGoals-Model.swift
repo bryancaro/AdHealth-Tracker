1c10f1699cba0d80ed191505411c92c5242a770c
Bryan Caro <bryan_caro@carrefour.com>
2023-02-26 20:12:47 +0100

    Add: new object film named 'health-model' has been added

---
+++ AdHealth Tracker/Shared/Models/HealthGoals-Model.swift
@@ -0,0 +1,89 @@
+//
+//  HealthGoals-Model.swift
+//  AdHealth Tracker
+//
+//  Created for AdHealth Tracker in 2023
+//  Using Swift 5.0
+//  Running on macOS 13.1
+//
+//  Created by Bryan Caro on 26/2/23.
+//  
+//
+
+import Foundation
+
+//  MARK: - RESPONSE
+struct HealthGoalsResponse: Decodable {
+    let goals: [GoalResponse]
+}
+
+struct GoalResponse: Decodable {
+    let id: Int
+    let goal: String
+    let description: String
+    let title: String
+    let type: String
+    let reward: RewardResponse
+}
+
+struct RewardResponse: Decodable {
+    let trophy: String
+    let points: String
+}
+
+//  MARK: - MODEL
+struct HealthGoalsModel {
+    let goals: [GoalModel]
+    
+    init(_ response: HealthGoalsResponse) {
+        self.goals = response.goals.map({ GoalModel($0) })
+    }
+    
+    init(goals: [GoalModel]) {
+        self.goals = goals
+    }
+}
+
+struct GoalModel {
+    let id: Int
+    let goal: String
+    let description: String
+    let title: String
+    let type: String
+    let reward: RewardModel
+    
+    init(_ response: GoalResponse) {
+        self.id = response.id
+        self.goal = response.goal
+        self.description = response.description
+        self.title = response.title
+        self.type = response.type
+        self.reward = RewardModel(response.reward)
+    }
+    
+    init(id: Int, goal: String, description: String, title: String, type: String, reward: RewardModel) {
+        self.id = id
+        self.goal = goal
+        self.description = description
+        self.title = title
+        self.type = type
+        self.reward = reward
+    }
+}
+
+struct RewardModel {
+    let trophy: String
+    let points: String
+    
+    init(_ response: RewardResponse) {
+        self.trophy = response.trophy
+        self.points = response.points
+    }
+    
+    init(trophy: String, points: String) {
+        self.trophy = trophy
+        self.points = points
+    }
+}
+
+//  MARK: - EXTENSION MODEL
